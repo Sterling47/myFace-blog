dc29ee538ded617c08f3962ba2b316a99a5c51c6
Adam <konber@gmail..com>
2023-11-20 12:22:24 -0700

    in proccess of updating route

---
+++ src/NewPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewPage = () => {
+  return (
+   <main>
+    <h1>NewPage</h1>
+   </main>
+  )
+}
+
+export default NewPage;
---
+++ src/NewPosts.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewPosts = () => {
+  return (
+    <main>
+        <h1>NewPost</h1>
+    </main>
+  )
+}
+
+export default NewPosts;
